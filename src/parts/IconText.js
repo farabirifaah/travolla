0e02c6b8e55ba0e2f387b69b24cacdf29f47ce52
Muhammad Farabby Rifaah <farabirifaah2@gmail.com>
2023-05-05 02:50:11 +0700

    Commit Component Variable Color
    tambahan compnent variable color

---
+++ src/parts/IconText.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Button from 'elements/Buttons'
+
+export default function IconText() {
+  return (
+    <Button className="brand-text-icon" href="" type="Link">
+      Stay <span className="text-grey-900">cation</span>
+    </Button>
+  )
+}
