d5c3f751818cc81d455baaa0608effb2183321e9
Muhammad Farabby Rifaah <farabirifaah2@gmail.com>
2023-05-05 23:54:46 +0700

    Commit Section Hero

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 
-// import moduleName from 'elements';
 import Header from 'parts/Header';
+import Hero from 'parts/Hero'
+
+import landingPage from 'json/landingPage.json';
+
 
 export default class LandingPage extends Component {
   render() {
     return (
       <>
         <Header {...this.props}></Header>
+        <Hero data={landingPage.hero}></Hero>
       </>
     )
   }
