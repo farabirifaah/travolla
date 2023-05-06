e492fea00cab947d25bf3acbd8b972cb880e7e6d
Muhammad Farabby Rifaah <farabirifaah2@gmail.com>
2023-05-06 18:30:17 +0700

    component categories

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import Header from "parts/Header";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
 
 import landingPage from "json/landingPage.json";
 
@@ -15,12 +16,13 @@ export default class LandingPage extends Component {
   render() {
     return (
       <>
-        <Header {...this.props}></Header>
-        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero}></Hero>
+        <Header {...this.props} />
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
         <MostPicked
           refMostPicked={this.refMostPicked}
           data={landingPage.mostPicked}
-        ></MostPicked>
+        />
+        <Categories data={landingPage.categories} />
       </>
     );
   }
