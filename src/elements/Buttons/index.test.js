357c073b4ad25bded133dddd2e0a78c33fc0655d
Muhammad Farabby Rifaah <farabirifaah2@gmail.com>
2023-05-21 04:11:03 +0700

    star testimony

--- src/elements/Buttons/index.test.js
+++ src/elements/Buttons/index.test.js
@@ -1,33 +1,32 @@
-import React from 'react';
+import React from "react";
 import { render, cleanup } from "@testing-library/react";
-import Button from './index'
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+import Button from "./index";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
+test("should not allowed click button if isDisabled is pressed", () => {
+  const { container } = render(<Button isDisabled></Button>);
 
-test('should not allowed click button if isDisabled is pressed', () => {
-  const { container } = render(<Button isDisabled></Button>)
-
-  expect(container.querySelector('span.disabled')).toBeInTheDocument();
+  expect(container.querySelector("span.disabled")).toBeInTheDocument();
 });
 
-test('should render loading / spinner', () => {
-  const { container, getByText } = render(<Button isLoading></Button>)
+test("should render loading / spinner", () => {
+  const { container, getByText } = render(<Button isLoading></Button>);
 
   expect(getByText(/Loading/i)).toBeInTheDocument();
 
-  expect(container.querySelector('span')).toBeInTheDocument();
+  expect(container.querySelector("span")).toBeInTheDocument();
 });
 
-test('should render <a> tag', () => {
-  const { container } = render(<Button type='Link' isExternal></Button>)
+test("should render <a> tag", () => {
+  const { container } = render(<Button type="link" isExternal></Button>);
 
-  expect(container.querySelector('a')).toBeInTheDocument();
+  expect(container.querySelector("a")).toBeInTheDocument();
 });
 
-test('should render <Link> Component', () => {
+test("should render <link> Component", () => {
   const { container } = render(
     <Router>
-      <Button href='' type='Link'></Button>
+      <Button href="" type="link"></Button>
     </Router>
   );
 
