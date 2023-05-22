848c127011281fa1f9e4ba4e01b3f5ec89d6df47
Muhammad Farabby Rifaah <farabirifaah2@gmail.com>
2023-05-23 03:06:05 +0700

    add new page detail

--- src/parts/PageDetailDescription.js
+++ src/parts/PageDetailDescription.js
@@ -4,7 +4,8 @@ import ReactHtmlParser from "react-html-parser";
 export default function PageDetailDescription({ data }) {
   return (
     <main>
-      <h4>About the palce</h4>
+      <h4>About the place</h4>
+      <br />
       {ReactHtmlParser(data.description)}
       <div className="row" style={{ marginTop: 30 }}>
         {data.features.map((feature, index) => {
