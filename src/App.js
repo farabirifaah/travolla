0d60dc35bfa8e1b0aaa78b87a73aafd35e50f257
Muhammad Farabby Rifaah <farabirifaah2@gmail.com>
2023-05-23 14:11:01 +0700

    finish slicing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "assets/scss/style.scss";
 import LandingPage from "pages/LandingPage";
 import DetailsPage from "pages/DetailsPage";
 import Example from "pages/Example";
+import Checkout from "pages/Checkout";
 // import EmptyPage from "pages/EmptyPage";
 
 // import EmptyPage from 'pages/EmptyPage';
@@ -16,6 +17,7 @@ function App() {
         <Route exact path="/" component={LandingPage}></Route>
         <Route path="/properties/:id" component={DetailsPage}></Route>
         <Route path="/example" component={Example}></Route>
+        <Route path="/checkout" component={Checkout}></Route>
         {/* <Route path="/empty-page" component={EmptyPage}></Route> */}
       </Router>
     </div>
